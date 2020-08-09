09e8b57b8614b1b7faf5e4d108a3c82d2b3bcef4
beyondseeker <makoto.sato@objectfanatics.com>
2020-08-10 08:58:10 +0900

    update

--- app/src/main/java/com/objectfanatics/chrono0019/MainActivity.kt
+++ app/src/main/java/com/objectfanatics/chrono0019/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.objectfanatics.chrono0019
 
 import android.Manifest
-import android.content.ContentValues
 import android.graphics.Bitmap
 import android.graphics.Bitmap.CompressFormat
 import android.graphics.Canvas
@@ -9,20 +8,13 @@ import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment.DIRECTORY_PICTURES
-import android.os.Environment.getExternalStoragePublicDirectory
-import android.provider.MediaStore
-import android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
 import android.view.View
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.objectfanatics.commons.android.provideer.mediastore.saveOnApi28OrOlder
+import com.objectfanatics.commons.android.provideer.mediastore.saveOnApi29OrNewer
 import permissions.dispatcher.NeedsPermission
 import permissions.dispatcher.RuntimePermissions
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.*
 
 @RuntimePermissions
 class MainActivity : AppCompatActivity() {
@@ -42,20 +34,32 @@ class MainActivity : AppCompatActivity() {
     private fun save(bitmap: Bitmap) {
         saveAndroidIconButton.isEnabled = false
 
+        val doOnSuccess = Runnable { }
+        val doOnError = Runnable { Toast.makeText(this@MainActivity, "エラーが発生しました", Toast.LENGTH_SHORT).show() }
+        val doOnEvent = Runnable { saveAndroidIconButton.isEnabled = true }
         when {
             // FIXME: 同じ引数仕様にするので、引数オブジェクト作って使いまわしたほうがいいと思われる。
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> saveOnApi29OrNewer(
-                bitmap = bitmap,
-                subDirectory = "chrono0019",
-                doOnError = Runnable { Toast.makeText( this@MainActivity, "エラーが発生しました", Toast.LENGTH_SHORT ).show() },
-                doOnEvent = Runnable { saveAndroidIconButton.isEnabled = true }
-            )
-            else -> saveOnApi28OrOlder(
-                bitmap = bitmap,
-                subDirectory = "chrono0019",
-                doOnError = Runnable { Toast.makeText( this@MainActivity, "エラーが発生しました", Toast.LENGTH_SHORT ).show() },
-                doOnEvent = Runnable { saveAndroidIconButton.isEnabled = true }
-            )
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q ->
+                saveOnApi29OrNewer(
+                    bitmap = bitmap,
+                    compressFormat = CompressFormat.PNG,
+                    standardDirectory = DIRECTORY_PICTURES,
+                    subDirectory = "chrono0019",
+                    doOnSuccess = doOnSuccess,
+                    doOnError = doOnError,
+                    doOnEvent = doOnEvent
+                )
+            else ->
+                saveOnApi28OrOlderWrapper(
+                    bitmap = bitmap,
+                    compressFormat = CompressFormat.PNG,
+                    quality = 100,
+                    standardDirectory = DIRECTORY_PICTURES,
+                    subDirectory = "chrono0019",
+                    doOnSuccess = doOnSuccess,
+                    doOnError = doOnError,
+                    doOnEvent = doOnEvent
+                )
         }
     }
 
@@ -72,190 +76,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    fun saveOnApi28OrOlder(
-        bitmap: Bitmap,
-        compressFormat: CompressFormat = CompressFormat.PNG,
-        quality: Int = 100,
-        standardDirectory: String = DIRECTORY_PICTURES,
-        subDirectory: String? = null,
-        // () -> Unit にしたいのだが、PermissionDispatcher のバグがあるためワークアラウンドをしている。
-        // https://github.com/permissions-dispatcher/PermissionsDispatcher/issues/503
-        doOnSuccess: Runnable = Runnable { },
-        doOnError: Runnable = Runnable { },
-        doOnEvent: Runnable = Runnable { }
-    ) {
-        assertApi28OrOlder()
-
-        val saveAsync: () -> Unit = {
-            val file = createExternalStorageFileOnApi28OrOlder(
-                standardDirectory,
-                compressFormat.extension,
-                subDirectory
-            )
-
-            @Suppress("DEPRECATION")
-            val values = ContentValues().apply {
-                put(MediaStore.Images.Media.MIME_TYPE, compressFormat.mimeType)
-                put(MediaStore.Images.Media.DATA, file.absolutePath)
-            }
-
-            val item = contentResolver.insert(EXTERNAL_CONTENT_URI, values)
-
-            if (item == null) {
-                doOnEvent.run()
-                doOnError.run()
-            } else {
-                FileOutputStream(file).use { os ->
-                    bitmap.compress(compressFormat, quality, os)
-                    os.flush()
-                }
-            }
-        }
-
-        execute(saveAsync, doOnSuccess, doOnError, doOnEvent)
-    }
-
-    // FIXME: これより上は一次精査完了 -----------------------------------------------------------------------
-
-    @RequiresApi(Build.VERSION_CODES.Q)
-    @Throws(IOException::class)
-    private fun saveOnApi29OrNewer(
-        bitmap: Bitmap,
-        compressFormat: CompressFormat = CompressFormat.PNG,
-        quality: Int = 100,
-        standardDirectory: String = DIRECTORY_PICTURES,
-        subDirectory: String? = null,
-        // () -> Unit にしたいのだが、PermissionDispatcher のバグがあるためワークアラウンドをしている。
-        // https://github.com/permissions-dispatcher/PermissionsDispatcher/issues/503
-        doOnSuccess: Runnable = Runnable { },
-        doOnError: Runnable = Runnable { },
-        doOnEvent: Runnable = Runnable { }
-    ) {
-        assertApi29OrNewer()
-
-        val saveAsync: () -> Unit = {
-            val values = ContentValues().apply {
-                put(
-                    MediaStore.Images.Media.DISPLAY_NAME,
-                    createDefaultImageFileName(compressFormat.extension)
-                )
-                put(
-                    MediaStore.Images.Media.RELATIVE_PATH,
-                    getRelativePath(standardDirectory, subDirectory)
-                )
-                put(MediaStore.Images.Media.MIME_TYPE, compressFormat.mimeType)
-                put(MediaStore.Images.Media.IS_PENDING, 1)
-            }
-
-            val externalContentUri =
-                MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-
-            val item = contentResolver.insert(externalContentUri, values)
-
-            if (item == null) {
-                runOnUiThread {
-                    doOnEvent.run()
-                    doOnError.run()
-                }
-            } else {
-                contentResolver.openFileDescriptor(item, "w", null).use {
-                    FileOutputStream(it!!.fileDescriptor).use { outputStream ->
-                        bitmap.compress(compressFormat, quality, outputStream)
-                        outputStream.flush()
-                    }
-                }
-                values.clear()
-                values.put(MediaStore.Images.Media.IS_PENDING, 0)
-                contentResolver.update(item, values, null, null)
-            }
-        }
-
-        execute(saveAsync, doOnSuccess, doOnError, doOnEvent)
-    }
-
-    // FIXME: これより下は精査済み ---------------------------------------------------------------------------
-    private fun execute(
-        save: () -> Unit,
-        doOnSuccess: Runnable,
-        doOnError: Runnable,
-        doOnEvent: Runnable
-    ) {
-        Thread(Runnable {
-            try {
-                save()
-            } catch (e: IOException) {
-                runOnUiThread {
-                    doOnEvent.run()
-                    doOnError.run()
-                }
-            }
-            runOnUiThread {
-                doOnEvent.run()
-                doOnSuccess.run()
-            }
-        }).start()
+    fun saveOnApi28OrOlderWrapper(bitmap: Bitmap, compressFormat: CompressFormat, quality: Int, standardDirectory: String, subDirectory: String?, doOnSuccess: Runnable, doOnError: Runnable, doOnEvent: Runnable) {
+        saveOnApi28OrOlder(bitmap, compressFormat, quality, standardDirectory, subDirectory, doOnSuccess, doOnError, doOnEvent)
     }
 }
-
-@Throws(IOException::class)
-fun createExternalStorageFileOnApi28OrOlder(
-    standardDirectory: String = DIRECTORY_PICTURES,
-    ext: String,
-    relativePath: String? = null
-): File {
-    assertApi28OrOlder()
-
-    // Deprecated in API level 29
-    @Suppress("DEPRECATION")
-    val externalStoragePublicDirectoryAbsolutePath =
-        getExternalStoragePublicDirectory(standardDirectory).absolutePath
-
-    val targetDir = File(
-        when (relativePath) {
-            null -> externalStoragePublicDirectoryAbsolutePath
-            else -> "$externalStoragePublicDirectoryAbsolutePath/$relativePath"
-        }
-    )
-
-    targetDir.mkdirs()
-
-    return File(targetDir.absolutePath, createDefaultImageFileName(ext))
-}
-
-fun createDefaultImageFileName(ext: String): String =
-    "IMG_${createDefaultTimestampString()}.$ext"
-
-/**
- * 端末の現在時刻を用いた "yyyyMMdd_HHmmss" フォーマットのタイムスタンプ文字列を返します。
- */
-fun createDefaultTimestampString(): String =
-    // FIXME: これって desugar して DateTimeFormatter 使いたいですよね。
-    SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
-
-private fun assertApi28OrOlder() {
-    if (BuildConfig.DEBUG && Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        error("This function is meant to be used by Android P or older.")
-    }
-}
-
-private fun assertApi29OrNewer() {
-    if (BuildConfig.DEBUG && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-        error("This function is meant to be used by Android Q or newer.")
-    }
-}
-
-private val CompressFormat.extension: String
-    get() = when (this) {
-        CompressFormat.JPEG -> "jpg"
-        CompressFormat.PNG -> "png"
-        CompressFormat.WEBP -> "webp"
-    }
-
-private val CompressFormat.mimeType: String
-    get() = "image/$extension"
-
-private fun getRelativePath(standardDirectory: String, subDirectory: String?): String =
-    when (subDirectory) {
-        null -> standardDirectory
-        else -> "$standardDirectory/$subDirectory"
-    }
---
+++ app/src/main/java/com/objectfanatics/commons/android/provideer/mediastore/package.kt
@@ -0,0 +1,210 @@
+package com.objectfanatics.commons.android.provideer.mediastore
+
+import android.Manifest
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Environment
+import android.os.Handler
+import android.os.Looper
+import android.provider.MediaStore
+import androidx.annotation.RequiresApi
+import com.objectfanatics.chrono0019.BuildConfig
+import permissions.dispatcher.NeedsPermission
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Throws(IOException::class)
+fun createExternalStorageFileOnApi28OrOlder(
+    standardDirectory: String = Environment.DIRECTORY_PICTURES,
+    ext: String,
+    relativePath: String? = null
+): File {
+    assertApi28OrOlder()
+
+    // Deprecated in API level 29
+    @Suppress("DEPRECATION")
+    val externalStoragePublicDirectoryAbsolutePath =
+        Environment.getExternalStoragePublicDirectory(standardDirectory).absolutePath
+
+    val targetDir = File(
+        when (relativePath) {
+            null -> externalStoragePublicDirectoryAbsolutePath
+            else -> "$externalStoragePublicDirectoryAbsolutePath/$relativePath"
+        }
+    )
+
+    targetDir.mkdirs()
+
+    return File(targetDir.absolutePath, createDefaultImageFileName(ext))
+}
+
+fun createDefaultImageFileName(ext: String): String =
+    "IMG_${createDefaultTimestampString()}.$ext"
+
+/**
+ * 端末の現在時刻を用いた "yyyyMMdd_HHmmss" フォーマットのタイムスタンプ文字列を返します。
+ */
+fun createDefaultTimestampString(): String =
+    // FIXME: これって desugar して DateTimeFormatter 使いたいですよね。
+    SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+
+fun assertApi28OrOlder() {
+    if (BuildConfig.DEBUG && Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        error("This function is meant to be used by Android P or older.")
+    }
+}
+
+fun assertApi29OrNewer() {
+    if (BuildConfig.DEBUG && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+        error("This function is meant to be used by Android Q or newer.")
+    }
+}
+
+val Bitmap.CompressFormat.extension: String
+    get() = when (this) {
+        Bitmap.CompressFormat.JPEG -> "jpg"
+        Bitmap.CompressFormat.PNG -> "png"
+        Bitmap.CompressFormat.WEBP -> "webp"
+    }
+
+val Bitmap.CompressFormat.mimeType: String
+    get() = "image/$extension"
+
+fun getRelativePath(standardDirectory: String, subDirectory: String?): String =
+    when (subDirectory) {
+        null -> standardDirectory
+        else -> "$standardDirectory/$subDirectory"
+    }
+
+fun post(run: () -> Unit) {
+    Handler(Looper.getMainLooper()).post { run() }
+}
+
+fun Context.saveOnApi28OrOlder(
+    bitmap: Bitmap,
+    compressFormat: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG,
+    quality: Int = 100,
+    standardDirectory: String = Environment.DIRECTORY_PICTURES,
+    subDirectory: String? = null,
+    // () -> Unit にしたいのだが、PermissionDispatcher のバグがあるためワークアラウンドをしている。
+    // https://github.com/permissions-dispatcher/PermissionsDispatcher/issues/503
+    doOnSuccess: Runnable = Runnable { },
+    doOnError: Runnable = Runnable { },
+    doOnEvent: Runnable = Runnable { }
+) {
+    assertApi28OrOlder()
+
+    val saveAsync: () -> Unit = {
+        val file = createExternalStorageFileOnApi28OrOlder(
+            standardDirectory,
+            compressFormat.extension,
+            subDirectory
+        )
+
+        @Suppress("DEPRECATION")
+        val values = ContentValues().apply {
+            put(MediaStore.Images.Media.MIME_TYPE, compressFormat.mimeType)
+            put(MediaStore.Images.Media.DATA, file.absolutePath)
+        }
+
+        val item = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        if (item == null) {
+            post {
+                doOnEvent.run()
+                doOnError.run()
+            }
+        } else {
+            FileOutputStream(file).use { os ->
+                bitmap.compress(compressFormat, quality, os)
+                os.flush()
+            }
+        }
+    }
+
+    execute(saveAsync, doOnSuccess, doOnError, doOnEvent)
+}
+
+@RequiresApi(Build.VERSION_CODES.Q)
+@Throws(IOException::class)
+fun Context.saveOnApi29OrNewer(
+    bitmap: Bitmap,
+    compressFormat: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG,
+    quality: Int = 100,
+    standardDirectory: String = Environment.DIRECTORY_PICTURES,
+    subDirectory: String? = null,
+    // () -> Unit にしたいのだが、PermissionDispatcher のバグがあるためワークアラウンドをしている。
+    // https://github.com/permissions-dispatcher/PermissionsDispatcher/issues/503
+    doOnSuccess: Runnable = Runnable { },
+    doOnError: Runnable = Runnable { },
+    doOnEvent: Runnable = Runnable { }
+) {
+    assertApi29OrNewer()
+
+    val saveAsync: () -> Unit = {
+        val values = ContentValues().apply {
+            put(
+                MediaStore.Images.Media.DISPLAY_NAME,
+                createDefaultImageFileName(compressFormat.extension)
+            )
+            put(
+                MediaStore.Images.Media.RELATIVE_PATH,
+                getRelativePath(standardDirectory, subDirectory)
+            )
+            put(MediaStore.Images.Media.MIME_TYPE, compressFormat.mimeType)
+            put(MediaStore.Images.Media.IS_PENDING, 1)
+        }
+
+        val externalContentUri =
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+
+        val item = contentResolver.insert(externalContentUri, values)
+
+        if (item == null) {
+            post {
+                doOnEvent.run()
+                doOnError.run()
+            }
+        } else {
+            contentResolver.openFileDescriptor(item, "w", null).use {
+                FileOutputStream(it!!.fileDescriptor).use { outputStream ->
+                    bitmap.compress(compressFormat, quality, outputStream)
+                    outputStream.flush()
+                }
+            }
+            values.clear()
+            values.put(MediaStore.Images.Media.IS_PENDING, 0)
+            contentResolver.update(item, values, null, null)
+        }
+    }
+
+    execute(saveAsync, doOnSuccess, doOnError, doOnEvent)
+}
+
+// FIXME: これより下は精査済み ---------------------------------------------------------------------------
+private fun execute(
+    save: () -> Unit,
+    doOnSuccess: Runnable,
+    doOnError: Runnable,
+    doOnEvent: Runnable
+) {
+    Thread(Runnable {
+        try {
+            save()
+        } catch (e: IOException) {
+            post {
+                doOnEvent.run()
+                doOnError.run()
+            }
+        }
+        post {
+            doOnEvent.run()
+            doOnSuccess.run()
+        }
+    }).start()
+}
